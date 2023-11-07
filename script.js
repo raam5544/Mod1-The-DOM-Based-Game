9bdf53de9f807f9f03e40173e4df672d23c9c9b3
raam <jeya.colombo@gmail.com>
2023-11-07 10:13:29 -0500

    Attempting to create plotting logic

--- script.js
+++ script.js
@@ -67,38 +67,55 @@ let x;
 let y;
 let attempt = 0
 
+const plotAreas = [[[164, 199], [144, 291], [191, 215]], [[116, 229], [227, 147], [95, 253]]]
+
 
 function fun() {
     board.append(plot)
-    x = 100 * getRandomInt(4);
-    y = 100 * getRandomInt(4);
+    x = getRandomInt(plotAreas.length - 1);
+    // y = getRandomInt(plotAreas.length - 1);
     plot.style.position = "absolute";
-    plot.style.left = x + 'px';
-    plot.style.top = y + 'px';
-    console.log(x)
-    console.log(y)
+    plot.style.left = plotAreas[x][x][1] + 'px';
+    plot.style.top = plotAreas[x][x][1] + 'px';
+    console.log(plot.style.left)
+    console.log(plot.style.top)
     playerScoreCalc()
     attempt++
     if (attempt == 3) {
         document.querySelector('#player1Butt').disabled = true
     }
+
 }
+
+// function playerScoreCalc() {
+
+//     if ((x < 248 && x > 154) && (y < 268 && y > 161)) {
+//         playerPoint = 15
+//     }
+//     else if (((x < 315 && x > 200) || (x > 85 && x < 150)) && ((y < 160 && y > 90) || (y > 260 && y < 330))) {
+//         playerPoint = 10
+//     }
+//     else if (((x < 395 && x > 320) || (x < 85 && x > 10)) && ((y < 85 && y > 10) || (y > 330 && y < 410))) {
+//         playerPoint = 5
+//     }
+//     player1HitPoint.textContent = `Player 1 Hit Point: ${playerPoint}`
+//     player1AccumulatedScore += playerPoint
+//     playerScoreAccum.textContent = `Player 1 Accu Score: ${player1AccumulatedScore}`
+//     console.log(playerPoint)
+// }
+const boardChild = document.querySelector('boardChild')
+const boardGrandChild = document.querySelector('boardGrandChild')
+
+
 
 function playerScoreCalc() {
 
-    if ((x < 248 && x > 154) && (y < 268 && y > 161)) {
-        playerPoint = 15
-    }
-    else if (((x < 315 && x > 200) || (x > 85 && x < 150)) && ((y < 160 && y > 90) || (y > 260 && y < 330))) {
-        playerPoint = 10
-    }
-    else if (((x < 395 && x > 320) || (x < 85 && x > 10)) && ((y < 85 && y > 10) || (y > 330 && y < 410))) {
-        playerPoint = 5
-    }
+
     player1HitPoint.textContent = `Player 1 Hit Point: ${playerPoint}`
     player1AccumulatedScore += playerPoint
     playerScoreAccum.textContent = `Player 1 Accu Score: ${player1AccumulatedScore}`
     console.log(playerPoint)
+
 }
 let isTwoPlayer = true
 
@@ -137,4 +154,7 @@ function roundUp() {
     if (round === 3) {
         document.querySelector('#roundUpButt').disabled = true
     }
+    console.log(board.getAttribute('coords'))
 }
+
+
